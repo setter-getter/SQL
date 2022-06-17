ec6573cfa77fa51558ec39d5f3f321efd9cca33b
setter_getter <57217944+setter-getter@users.noreply.github.com>
2022-06-17 22:18:36 +0300

    Update HW_2.sql

--- HW_2.sql
+++ HW_2.sql
@@ -1,8 +1,7 @@
---1. Создать таблицу employees
+--1. РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ employees
 -- - id. serial,  primary key,
 -- - employee_name. Varchar(50), not null
---2. Наполнить таблицу employee 70 строками.
-
+--2. РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ employee 70 СЃС‚СЂРѕРєР°РјРё.
 
 create table employees(
 id serial primary key,
@@ -10,51 +9,51 @@ employee_name varchar(50) not null);
 
 insert into employees(id, employee_name)
 values(default,'Thomas'),
-	  (default,'Michael'),
-	  (default,'Elliott'),
+      (default,'Michael'),
+      (default,'Elliott'),
       (default,'James'),
       (default,'Steven'),
       (default,'Anna_5'),
       (default,'Anna_7'),
       (default,'Anna_8'),
-	  (default,'Anna_9'),
+      (default,'Anna_9'),
       (default,'8_Jermaine'),
       (default,'8_Terence'),
       (default,'8_Mabel'),
       (default,'8_Mary'),
-	  (default,'8_Yoshie'),
-	  (default,'8_David'),
+      (default,'8_Yoshie'),
+      (default,'8_David'),
       (default,'8_Robert'),
       (default,'8_Stanley'),
       (default,'8_James'),
       (default,'8_Patty'),
       (default,'8_Jackson'),
-	  (default,'8_Frank'),
+      (default,'8_Frank'),
       (default,'8_William'),
       (default,'8_Earl'),
       (default,'8_Sylvia'),
       (default,'8_Kathleen'),
       (default,'8_Dallas'),
-	  (default,'8_Tamera'),
-	  (default,'Andrey_1'),
+      (default,'8_Tamera'),
+      (default,'Andrey_1'),
       (default,'Andrey_2'),
       (default,'Andrey_3'),
       (default,'Andrey_4'),
       (default,'Andrey_5'),
       (default,'Andrey_6'),
-	  (default,'Leticia_Long'),
+      (default,'Leticia_Long'),
       (default,'Christopher_Kumro'),
       (default,'Olive_Warne'),
       (default,'Kenneth_Coleman'),
       (default,'William_Owen'),
-	  (default,'Richard_Laflam'),
-	  (default,'Bettye_Bevington'),
+      (default,'Richard_Laflam'),
+      (default,'Bettye_Bevington'),
       (default,'Frida_Krieger'),
       (default,'Rocky_Siddon'),
       (default,'Veronica_Jones'),
       (default,'Michael_Carrano'),
       (default,'Joanne_Hubler'),
-	  (default,'Thomas_Patel'),
+      (default,'Thomas_Patel'),
       (default,'Angelina_Venezia'),
       (default,'Albert_Madrigal'),
       (default,'Alice_Garner'),
@@ -62,19 +61,19 @@ values(default,'Thomas'),
       (default,'Derrick_Gary'),
       (default,'Pauline_Moody'),
       (default,'John_Perez'),
-	  (default,'Philip_Wilde'),
+      (default,'Philip_Wilde'),
       (default,'Debra_Holland'),
       (default,'Amy_Mcmanus'),
       (default,'Karl_Storie'),
       (default,'Christine_Ackland'),
-	  (default,'James_Bradford'),
-	  (default,'Sadie_Mcdougall'),
+      (default,'James_Bradford'),
+      (default,'Sadie_Mcdougall'),
       (default,'Gerald_Gross'),
       (default,'Frederick_Bumbrey'),
       (default,'Abbey_Tracy'),
       (default,'Michael_Carrano'),
       (default,'Bryan_Miller'),
-	  (default,'Anita_Frederick'),
+      (default,'Anita_Frederick'),
       (default,'Ernest_Wood'),
       (default,'Mary_Tornow'),
       (default,'Marion_Daniel'),
@@ -82,11 +81,10 @@ values(default,'Thomas'),
 
 
 
---3. Создать таблицу salary
+--3. РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ salary
 -- - id. Serial  primary key,
 -- - monthly_salary. Int, not null
--- 4.Наполнить таблицу salary 15 строками:
-
+-- 4.РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ salary 15 СЃС‚СЂРѕРєР°РјРё:
 
 create table salary(
 id serial primary key,
@@ -94,30 +92,29 @@ monthly_salary int not null);
 
 insert into salary(monthly_salary)
 values(1000),
-	  (1100),
-	  (1200),
-	  (1300),
-	  (1400),
-	  (1500),
-	  (1600),
-	  (1700),
-	  (1800),
-	  (1900),
-	  (2000),
-	  (2100),
-	  (2200),
-	  (2300),
-	  (2400),
-	  (2500);
+      (1100),
+      (1200),
+      (1300),
+      (1400),
+      (1500),
+      (1600),
+      (1700),
+      (1800),
+      (1900),
+      (2000),
+      (2100),
+      (2200),
+      (2300),
+      (2400),
+      (2500);
 
 
-
--- 5.Создать таблицу employee_salary
+-- 5.РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ employee_salary
 -- - id. Serial  primary key,
 -- - employee_id. Int, not null, unique
 -- - salary_id. Int, not null
--- 6.Наполнить таблицу employee_salary 40 строками:
--- - в 10 строк из 40 вставить несуществующие employee_id
+-- 6.РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ employee_salary 40 СЃС‚СЂРѕРєР°РјРё:
+-- - РІ 10 СЃС‚СЂРѕРє РёР· 40 РІСЃС‚Р°РІРёС‚СЊ РЅРµСЃСѓС‰РµСЃС‚РІСѓСЋС‰РёРµ employee_id
 
 create table employee_salary(
 id serial primary key,
@@ -125,58 +122,57 @@ employee_id int not null unique,
 salary_id int not null);
 
 
-
 insert into employee_salary(employee_id, salary_id)
 values(1,1),
-  	  (2,2),
-  	  (3,3),
-  	  (4,4),
-  	  (5,5),
-  	  (6,6),
-  	  (7,7),
-  	  (8,8),
-  	  (9,9),
-  	  (10,10),
-  	  (11,11),
-  	  (12,12),
-  	  (13,13),
-  	  (14,14),
-  	  (15,15),
-  	  (16,15),
-  	  (17,14),
-  	  (18,13),
-  	  (19,12),
-  	  (20,11),
-  	  (21,10),
-  	  (22,9),
-  	  (23,8),
-  	  (24,7),
-  	  (25,6),
-  	  (26,5),
-  	  (27,4),
-  	  (28,3),
-  	  (29,1),
-  	  (30,16),
-  	  (71,1),
-  	  (72,2),
-	  (73,3),
-  	  (74,4),
-  	  (75,5),
-  	  (76,6),
-  	  (77,7),
-  	  (78,8),
-  	  (79,9),
-  	  (80,10);
+      (2,2),
+      (3,3),
+      (4,4),
+      (5,5),
+      (6,6),
+      (7,7),
+      (8,8),
+      (9,9),
+      (10,10),
+      (11,11),
+      (12,12),
+      (13,13),
+      (14,14),
+      (15,15),
+      (16,15),
+      (17,14),
+      (18,13),
+      (19,12),
+      (20,11),
+      (21,10),
+      (22,9),
+      (23,8),
+      (24,7),
+      (25,6),
+      (26,5),
+      (27,4),
+      (28,3),
+      (29,1),
+      (30,16),
+      (71,1),
+      (72,2),
+      (73,3),
+      (74,4),
+      (75,5),
+      (76,6),
+      (77,7),
+      (78,8),
+      (79,9),
+      (80,10);
   	  
   	 
---  7. Создать таблицу roles
+--  7. РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ roles
 -- - id. Serial  primary key,
 -- - role_name. int, not null, unique
--- 8.Поменять тип столба role_name с int на varchar(30)
--- 9. Наполнить таблицу roles 20 строками:
+-- 8.РџРѕРјРµРЅСЏС‚СЊ С‚РёРї СЃС‚РѕР»Р±Р° role_name СЃ int РЅР° varchar(30)
+-- 9. РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ roles 20 СЃС‚СЂРѕРєР°РјРё:
 	 
   	 
- create table roles (
+create table roles (
 id serial primary key,
 role_name int unique not null
 );
@@ -185,32 +181,32 @@ alter table roles alter column role_name type varchar(30);
 
 insert into roles(role_name)
 values('Junior Python developer'),
-	  ('Middle Python developer'),
-	  ('Senior Python developer'),
-	  ('Junior Java developer'),
-	  ('Middle Java developer'),
-	  ('Senior Java developer'),
-	  ('Junior JavaScript developer'),
-	  ('Middle JavaScript developer'),
-	  ('Senior JavaScript developer'),
-	  ('Junior Manual QA engineer'),
-	  ('Senior Manual QA engineer'),
-	  ('Project Manager'),
-	  ('Designer'),
-	  ('HR'),
-	  ('CEO'),
-	  ('Sales manager'),
-	  ('Junior Automation QA engineer'),
-	  ('Middle Automation QA engineer'),
-	  ('Senior Automation QA engineer');
+      ('Middle Python developer'),
+      ('Senior Python developer'),
+      ('Junior Java developer'),
+      ('Middle Java developer'),
+      ('Senior Java developer'),
+      ('Junior JavaScript developer'),
+      ('Middle JavaScript developer'),
+      ('Senior JavaScript developer'),
+      ('Junior Manual QA engineer'),
+      ('Senior Manual QA engineer'),
+      ('Project Manager'),
+      ('Designer'),
+      ('HR'),
+      ('CEO'),
+      ('Sales manager'),
+      ('Junior Automation QA engineer'),
+      ('Middle Automation QA engineer'),
+      ('Senior Automation QA engineer');
 	    
 
 
--- 10.Создать таблицу roles_employee
+-- 10.РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ roles_employee
 -- - id. Serial  primary key,
--- - employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
--- - role_id. Int, not null (внешний ключ для таблицы roles, поле id)
--- 11.Наполнить таблицу roles_employee 40 строками
+-- - employee_id. Int, not null, unique (РІРЅРµС€РЅРёР№ РєР»СЋС‡ РґР»СЏ С‚Р°Р±Р»РёС†С‹ employees, РїРѕР»Рµ id)
+-- - role_id. Int, not null (РІРЅРµС€РЅРёР№ РєР»СЋС‡ РґР»СЏ С‚Р°Р±Р»РёС†С‹ roles, РїРѕР»Рµ id)
+-- 11.РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ roles_employee 40 СЃС‚СЂРѕРєР°РјРё
 
 create table  roles_employee(
 id serial primary key,
